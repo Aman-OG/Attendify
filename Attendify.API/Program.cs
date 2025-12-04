101612f42b66bcfb93158fd5b24418b4a6e05813
Aman <amanbaye4@gmail.com>
2025-12-04 07:38:14 -0800

    initializing a supabase database

--- Attendify.API/Program.cs
+++ Attendify.API/Program.cs
@@ -1,12 +1,39 @@
+
+
+
+using Attendify.DATA;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+
+
+var supabaseUrl = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var supabaseKey = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Load .env
+DotNetEnv.Env.Load();
+var connectionString = Environment.GetEnvironmentVariable("SUPABASE_KEY"); 
+var dbUrl = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var fullConnectionString = "Host=YOUR_HOST;Port=5432;Database=YOUR_DB;Username=YOUR_USER;Password=YOUR_PASS";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(fullConnectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
+
+builder.Services.AddOpenApi();
 var app = builder.Build();
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
