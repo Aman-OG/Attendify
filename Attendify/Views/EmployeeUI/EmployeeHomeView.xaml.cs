b1a38c2221d4397354dcd626e660f793f1f5dc03
Aman <amanbaye4@gmail.com>
2025-12-13 11:55:41 -0800

    started working on employee home

--- Attendify/Views/EmployeeUI/EmployeeHomeView.xaml.cs
+++ Attendify/Views/EmployeeUI/EmployeeHomeView.xaml.cs
@@ -10,25 +10,6 @@ namespace Attendify.Views.Employee
             InitializeComponent();
         }
 
-        private void ClockInOut_Click(object sender, RoutedEventArgs e)
-        {
-            // TODO: Implement clock in/out functionality
-            MessageBox.Show("Clock In/Out functionality coming soon!", "Clock Action",
-                MessageBoxButton.OK, MessageBoxImage.Information);
-        }
 
-        private void RequestLeave_Click(object sender, RoutedEventArgs e)
-        {
-            // TODO: Navigate to leave requests or open leave request dialog
-            MessageBox.Show("Leave request functionality coming soon!", "Leave Request",
-                MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-
-        private void ViewSchedule_Click(object sender, RoutedEventArgs e)
-        {
-            // TODO: Navigate to shifts view or open schedule dialog
-            MessageBox.Show("Schedule view functionality coming soon!", "View Schedule",
-                MessageBoxButton.OK, MessageBoxImage.Information);
-        }
     }
 }
